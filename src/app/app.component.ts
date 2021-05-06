8ee567c0d1ecf8b1c25dca9b7fe4f15a9d678f42
Matajar <info.matajar@gmail.com>
2021-05-06 11:31:25 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-dynamic-form-builder';
+export class AppComponent implements OnInit {
+  template: any[] = [
+    {
+      type: 'text',
+      label: 'Input',
+    },
+  ];
+
+  myFormGroup: FormGroup;
+  allTemplate: any = this.template;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.formBuilder();
+  }
+
+  formBuilder() {
+    let group: any = {};
+    this.allTemplate.forEach((input_template: any) => {
+      group[input_template.label] = new FormControl('');
+    });
+    this.myFormGroup = new FormGroup(group);
+  }
+
+  onSubmit() {
+    console.log(this.myFormGroup.value);
+  }
+
+  addnewInput() {
+    let newTemplate = [
+      {
+        type: 'text',
+        label: 'Input' + (this.allTemplate.length + 1),
+      },
+    ];
+
+    this.allTemplate = this.allTemplate.concat(newTemplate);
+    this.formBuilder();
+  }
 }
